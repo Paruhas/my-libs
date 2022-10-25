8e7c2a7ceb5925fad10c18701050f8b6b62b0982
LENOVO-Y530\BANK <bayulgang@hotmail.com>
2022-10-25 20:05:03 +0700

    update my libs

---
+++ app.js
@@ -0,0 +1,5 @@
+const Day = require("./libs/Day");
+const Decimal = require("./libs/Decimal");
+const tryJSON = require("./libs/tryJSON");
+const UuidV4 = require("./libs/UuidV4");
+const Validator = require("./libs/Validator");
